e339d8db185955262e87d73c4f738f01a34121bd
evgenius2049 <evgenius2049@tutanota.com>
2022-08-21 00:55:45 +0300

    регистрация модели Todo в todo_backend_app.admin

--- todo_backend_app/admin.py
+++ todo_backend_app/admin.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use Python 3.10.0
+
+Панель администратора
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    """
+    Отображение модели Todo
+    """
+    list_display = ('id', 'owner', 'title', 'description',
+                    'done', 'created_date', 'modified_date', 'version', )
+    list_display_links = ('id', 'title', )
+    search_fields = ('id', 'title', 'description', )
+    list_editable = ('done', )
+    list_filter = ('owner', 'done', )
+
+
+admin.site.register(Todo, TodoAdmin)
