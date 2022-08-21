cf7c775ef08734f1a01a3e540704b9d34f512f8e
evgenius2049 <evgenius2049@tutanota.com>
2022-08-21 15:50:45 +0300

    реализация деаутентификации LogoutAPIView в todo_backend_app.views

--- todo_backend_app/urls.py
+++ todo_backend_app/urls.py
@@ -10,12 +10,15 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import (
+    LogoutAPIView,
     TodoListCreateAPIView,
 )
 
 
 urlpatterns = [
-    path(route='auth/token/', view=obtain_auth_token, name='auth-token-get'),
+    path(route='auth/token/get/', view=obtain_auth_token, name='auth-token-get'),
+    path(route='auth/token/remove/', view=LogoutAPIView.as_view(),
+         name='auth-token-remove'),
     path(route='todo/', view=TodoListCreateAPIView.as_view(),
          name='todo-list-create'),
 ]
--- todo_backend_app/views.py
+++ todo_backend_app/views.py
@@ -6,7 +6,9 @@ Use Python 3.10.0
 Представления rest_framework.generics
 """
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Todo
 from .serializers import TodoSerializer
@@ -19,3 +21,14 @@ class TodoListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = [
         permissions.IsAuthenticated,
     ]
+
+
+class LogoutAPIView(APIView):
+
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    def post(self, request, format=None):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
